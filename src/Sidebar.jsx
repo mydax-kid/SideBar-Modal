d8969ee19f89d1077cc73aa7fdcc67f5af237bc8
mydax <41824317+mydax-kid@users.noreply.github.com>
2023-01-11 14:53:52 +0000

    Initial commit

---
+++ src/Sidebar.jsx
@@ -0,0 +1,51 @@
+//import logo from './logo.svg'
+import {social, links} from './data'
+import {FaTimes} from 'react-icons/fa'
+import {useGlobalContext} from './context'
+
+
+const Sidebar = () => {
+  const {isSidebarOpen, closeSidebar} = useGlobalContext();
+
+  
+  return(
+    <aside className= { `sidebar ${isSidebarOpen ? 'show-sidebar' : null}` }>
+      <div className= 'sidebar-header'>
+        <div className= 'logo'></div>
+        <button className= 'close-btn' onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+      </div>
+      
+      <ul className= 'links'>
+        {links.map(link => {
+          const {id, url, text, icon} = link;
+          return(
+            <li key={id}>
+              <a href={url}>
+                {icon}
+                {text}
+              </a>
+            </li>
+          )
+        })}
+      </ul>
+
+      <ul className= 'social-icons'>
+        {social.map(item => {
+        const {id, url, icon} = item;
+        return(
+          <li key= {id}>
+            <a href= {url}>
+              {icon}
+            </a>
+          </li>
+        )
+        })}
+      </ul>
+      
+    </aside>
+  )
+}
+
+export default Sidebar;
